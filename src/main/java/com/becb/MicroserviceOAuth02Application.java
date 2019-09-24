94480edb6aa4eb3a11a3ccdeec3178787c09fc8e
Fred Belisario <fred.belisario@gmail.com>
2019-09-23 21:51:19 -0300

    arquivos basicos

---
+++ src/main/java/com/becb/MicroserviceOAuth02Application.java
@@ -0,0 +1,13 @@
+package com.becb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceOAuth02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceOAuth02Application.class, args);
+	}
+
+}
